c6dce3c4286e3fe59ed87ce56450fcf149a87385
José Adolfo Galdámez <josegalre@pac-rom.com>
2015-11-14 20:58:31 -0600

    arc: version ocd
    Change-Id: Idc7e85637716c19cde414b82e062bb2deee4ac96

--- config/arc_config.mk
+++ config/arc_config.mk
@@ -1,6 +1,8 @@
 
 # Version
 ARC_BASE := Marshmallow
-ARC_CODE := 0.1-Alpha
-ARC_VERSION := $(ARC_BASE)_$(ARC_CODE)
-PRODUCT_PROPERTY_OVERRIDES += ro.arc.version=$(ARC_VERSION)
+ARC_CODE := 0.1
+ARC_TYPE := Official
+ARC_LEVEL := Alpha
+ARC_VERSION := $(ARC_BASE) $(ARC_CODE) $(ARC_LEVEL) $(ARC_TYPE)
+ARC_ZIP_NAME := $(ARC_BASE)-$(ARC_CODE)-$(ARC_LEVEL)-$(shell date -u +%Y%m%d)-$(ARC_TYPE)-$(CM_BUILD)
