ed10cba32725aca74c79aa23601ad96e0479fbf9
José Adolfo Galdámez <josegalre@pac-rom.com>
2016-01-05 19:32:36 -0600

    vendor: add KernelAdiutor from GooglePlay
    Change-Id: I687a3ace8ad3164f028e891fe086f7138e761812

--- config/arc_config.mk
+++ config/arc_config.mk
@@ -6,3 +6,7 @@ ARC_TYPE := Official
 ARC_LEVEL := Alpha
 ARC_VERSION := $(ARC_BASE) $(ARC_CODE) $(ARC_LEVEL) $(ARC_TYPE)
 ARC_ZIP_NAME := $(ARC_BASE)-$(ARC_CODE)-$(ARC_LEVEL)-$(shell date -u +%Y%m%d)-$(ARC_TYPE)-$(CM_BUILD)
+
+# Packages
+PRODUCT_PACKAGES += \
+    KernelAdiutor
